fee30d0c0e455b0d3f9c037e7d921c55a688e0f7
marcialino <marcialino100@gmail.com>
2023-06-07 10:52:18 -0300

    atualizei

--- mario_jogo/js/script.js
+++ mario_jogo/js/script.js
@@ -13,22 +13,23 @@ const jump = ()=>{
 const loop = setInterval(()=>{
 
     const pipePosition = pipe.offsetLeft
+    
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    
 
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('p', '')
-
-    if(pipePosition <=120 && pipePosition > 0 && marioPosition < 80){
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
         pipe.style.animation = 'none'
         pipe.style.left =`${pipePosition}px `
 
         mario.style.animation = 'none'
-        mario.style.bottom =`${marioPosition}px `
+        mario.style.bottom =`${marioPosition}px`
 
-        mario.src = './imagens/mario1.jpg'
+        mario.src = "./imagens/mario1.jpg"
         mario.style.width = '75px'
         mario.style.marginLeft ='50px'
 
         clearInterval(loop)
-
+        
     }
 
 }, 10)
